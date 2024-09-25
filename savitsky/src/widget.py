a6044814f4b8e0f80f7655d097d080a2d1500577
Alexandr Savitsky <savitsky.alexandr@mail.ru>
2024-09-25 07:22:50 +0300

    after check

--- savitsky/src/widget.py
+++ savitsky/src/widget.py
@@ -7,21 +7,21 @@ def mask_account_card(values: str) -> str:
         result = f"{number[:4]}{number[4:6]}** **** {number[12:]}"
     elif len(number) == 20:
         result = f"**{number[-4:]}"
-    print(values.split()[0], result)
+    return f"{values.split()[0]} {result}"
 
 
-mask_account_card('Maestro 1596837868705199')
-mask_account_card('Счет 64686473678894779589')
-mask_account_card('MasterCard 7158300734726758')
-mask_account_card('Счет 35383033474447895560')
-mask_account_card('Visa Classic 6831982476737658')
-mask_account_card('Visa Platinum 8990922113665229')
-mask_account_card('Visa Gold 5999414228426353')
-mask_account_card('Счет 73654108430135874305')
+print(mask_account_card('Maestro 1596837868705199'))
+print(mask_account_card('Счет 64686473678894779589'))
+print(mask_account_card('MasterCard 7158300734726758'))
+print(mask_account_card('Счет 35383033474447895560'))
+print(mask_account_card('Visa Classic 6831982476737658'))
+print(mask_account_card('Visa Platinum 8990922113665229'))
+print(mask_account_card('Visa Gold 5999414228426353'))
+print(mask_account_card('Счет 73654108430135874305'))
 
 def get_date(user_date: str) -> str:
  """Функция получения даты в определенном формате и
           возвращения в формате ДД.ММ.ГГГГ"""
 
- return f"'ДД.ММ.ГГГГ'{user_date[8:10]}.{user_date[5:7]}.{user_date[0:4]}"
+ return f"{user_date[8:10]}.{user_date[5:7]}.{user_date[0:4]}"
 print(get_date("2024-03-11T02:26:18.671407"))
