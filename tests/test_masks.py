c41c44bdb8ad71e6c724a1f333f17954cbaaae59
Alexandr Savitsky <savitsky.alexandr@mail.ru>
2024-10-02 21:10:53 +0300

    commit with linters

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,19 +1,25 @@
 import pytest
-from src.masks import get_mask_card_number, get_mask_account
+
+from src.masks import get_mask_account, get_mask_card_number
 
 
-@pytest.mark.parametrize("string, expected_result", [
-    ("7158300734726758", "7158 30** **** 6758"),
-    ("7158300734726759", "7158 30** **** 6759"),
-])
-def test_get_mask_card_number(string, expected_result):
+@pytest.mark.parametrize(
+    "string, expected_result",
+    [
+        ("7158300734726758", "7158 30** **** 6758"),
+        ("7158300734726759", "7158 30** **** 6759"),
+    ],
+)
+def test_get_mask_card_number(string, expected_result):    # type: ignore[no-untyped-def]
     assert get_mask_card_number(string) == expected_result
 
 
-@pytest.mark.parametrize("string, expected_result", [
-    ("12345678901234567340", "**7340"),
-    ("12345678901234567890", "**7890"),
-])
-def test_get_mask_account(string, expected_result):
+@pytest.mark.parametrize(
+    "string, expected_result",
+    [
+        ("12345678901234567340", "**7340"),
+        ("12345678901234567890", "**7890"),
+    ],
+)
+def test_get_mask_account(string, expected_result):     # type: ignore[no-untyped-def]
     assert get_mask_account(string) == expected_result
-
