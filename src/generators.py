a3013847bb4e6e176f1b4b8ee2c59b2f3fd97094
Alexandr Savitsky <savitsky.alexandr@mail.ru>
2024-10-05 23:21:51 +0300

    all tasks done

--- src/generators.py
+++ src/generators.py
@@ -1,28 +1,36 @@
-def filter_by_currency():
-    """Функция возвращает итератор, который поочередно
-     выдает транзакции, где валюта операции соответствует заданной"""
-    pass
+from typing import Generator
 
-usd_transactions = filter_by_currency(transactions, "USD")
-for _ in range(2):
-    print(next(usd_transactions))
 
-def transaction_description():
+def filter_by_currency(transaction_list: list, currency: str) -> Generator:
+    """Функция возвращает итератор, который поочередно выдает транзакции,
+     где валюта операции соответствует заданной """
+    if len(transaction_list) > 0:
+        for transaction in transaction_list:
+            if transaction.get("operationAmount", [{}]).get("currency", [{}]).get("code", [{}]) == currency:
+                yield transaction
+    else:
+        yield []
+
+
+def transaction_descriptions(transaction_list: list) -> Generator:
     """Генератор, который принимает список словарей с транзакциями
-     и возвращает описание каждой операции по очереди"""
-    pass
+         и возвращает описание каждой операции по очереди"""
+    if len(transaction_list) > 0:
+        for transaction in transaction_list:
+            yield transaction.get("description")
+    else:
+        yield []
 
-descriptions = transaction_descriptions(transactions)
-for _ in range(5):
-    print(next(descriptions))
 
-   # Перевод организации
-   # Перевод со счета на счет
-   # Перевод со счета на счет
-   # Перевод с карты на карту
-   # Перевод организации
-
-card_number_generator = ()
-for card_number in card_number_generator(1, 5):
-    print(card_number)
-
+def card_number_generator(start: int, stop: int) -> Generator:
+    """Создаём генератор , который выдает номера банковских карт в
+    формате XXXX XXXX XXXX XXXX, где X — цифра номера карты."""
+    if start == 0 and stop > 0:
+        start += 1
+    for number in range(start, stop):
+        card_number = str(number)
+        while len(card_number) < 16:
+            if start < 1000000000000000:
+                card_number = '0' + card_number
+        formatted_card_number = f"{card_number[0:4]} {card_number[4:8]} {card_number[8:12]} {card_number[12:16]}"
+        yield formatted_card_number
