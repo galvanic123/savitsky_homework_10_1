c4ed2fac202db258980395139d2fa6844fa6a6bb
Alexandr Savitsky <savitsky.alexandr@mail.ru>
2024-10-08 21:27:35 +0300

    add decorators & my_log

--- src/decorators.py
+++ src/decorators.py
@@ -1,4 +1,34 @@
 from collections.abc import Callable
 from functools import wraps
-from typing import Union, Any
+from typing import Any
+
+
+def log(filename: str | None = None):
+    """Логирует вызов функции и её результат в файл или в консоль"""
+
+    def my_decorator(func: Callable) -> Callable:
+        @wraps(func)
+        def wrapper(*args: Any, **kwargs: Any) -> Any:
+            try:
+                result = func(*args, **kwargs)
+                if filename:
+                    with open(filename, 'a') as file:
+                        file.write(f"{func.__name__} ok")
+                else:
+                    print(f"{func.__name__} ok ")
+            except Exception as e:
+                if filename:
+                    with open(filename, 'a') as file:
+                        file.write(f"{func.__name__} error: {e}. Input: {args}, {kwargs}")
+                else:
+                    print(f"{func.__name__} error: {e}. Input: {args}, {kwargs}")
+            return result
+        return wrapper
+    return my_decorator
+
+@log(filename='my_log.txt')
+def my_function(x: int, y: int) -> int:
+    return x + y
+
+print(my_function(1, 2))
 
