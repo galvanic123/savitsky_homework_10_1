bbeba3324a086fda194a6ce055e43f5b6af7d5de
Alexandr Savitsky <savitsky.alexandr@mail.ru>
2024-10-08 23:14:47 +0300

    final commit with html, flake8, mypy, isort, black

--- src/decorators.py
+++ src/decorators.py
@@ -3,32 +3,35 @@ from functools import wraps
 from typing import Any
 
 
-def log(filename: str | None = None):
+def log(filename: Any = None) -> Any:
     """Логирует вызов функции и её результат в файл или в консоль"""
 
-    def my_decorator(func: Callable) -> Callable:
+    def decorator(func: Callable) -> Callable:
         @wraps(func)
         def wrapper(*args: Any, **kwargs: Any) -> Any:
             try:
                 result = func(*args, **kwargs)
                 if filename:
-                    with open(filename, 'a') as file:
-                        file.write(f"{func.__name__} ok")
+                    with open(filename, "a") as file:
+                        file.write(f"{func.__name__} ok\n")
                 else:
-                    print(f"{func.__name__} ok ")
+                    print(f"{func.__name__} ok\n")
             except Exception as e:
                 if filename:
-                    with open(filename, 'a') as file:
-                        file.write(f"{func.__name__} error: {e}. Input: {args}, {kwargs}")
+                    with open(filename, "a") as file:
+                        file.write(f"{func.__name__}\n error: {e}. Input: {args}, {kwargs}")
                 else:
-                    print(f"{func.__name__} error: {e}. Input: {args}, {kwargs}")
+                    print(f"{func.__name__}\n error: {e}. Input: {args}, {kwargs}")
             return result
-        return wrapper
-    return my_decorator
 
-@log(filename='my_log.txt')
+        return wrapper
+
+    return decorator
+
+
+@log(filename=None)
 def my_function(x: int, y: int) -> int:
     return x + y
 
-print(my_function(1, 2))
 
+my_function(1, 2)
